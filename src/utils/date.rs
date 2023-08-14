88685a491f86d2d23407efd665270ac66956b4ef
tikitko <bns.6587@gmail.com>
2023-08-15 02:15:28 +0300

    rus date

--- src/utils/date.rs
+++ src/utils/date.rs
@@ -1,9 +1,9 @@
-use chrono::{Local, TimeZone, Utc};
+use chrono::{Local, TimeZone, Utc, Locale};
 
 pub fn format(milliseconds: u64) -> String {
     let dt = Utc
         .timestamp_opt((milliseconds as i64) / 1000, 0)
         .unwrap()
         .with_timezone(&Local);
-    dt.format("%b %e %H:%M %Y").to_string()
+    dt.format_localized("%H:%M | %e %B %Y", Locale::ru_RU).to_string()
 }
