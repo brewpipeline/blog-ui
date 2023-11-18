3b2eb2552a953d06d074b4ffcb9102156631326b
Mikita Bykau <bns.6587@gmail.com>
2023-11-18 22:44:28 +0100

    Open-Graph-meta (#44)

--- src/components/meta.rs
+++ src/components/meta.rs
@@ -6,11 +6,15 @@ use crate::utils::head::*;
 #[derive(PartialEq, Properties, Clone)]
 pub struct MetaProps {
     #[prop_or_default]
+    pub r#type: String,
+    #[prop_or_default]
     pub title: String,
     #[prop_or_default]
     pub description: String,
     #[prop_or_default]
     pub keywords: String,
+    #[prop_or_default]
+    pub image: String,
     #[prop_or_default]
     pub noindex: bool,
 }
@@ -18,11 +22,14 @@ pub struct MetaProps {
 #[function_component(Meta)]
 pub fn meta(props: &MetaProps) -> Html {
     let MetaProps {
+        r#type,
         title,
         description,
         keywords,
+        image,
         noindex,
     } = props.clone();
+    let r#type = r#type.none_if_empty().unwrap_or("website".to_owned());
     let title = title
         .none_if_empty()
         .map(|t| format!("{} - {}", t, crate::TITLE))
@@ -33,17 +40,28 @@ pub fn meta(props: &MetaProps) -> Html {
     let keywords = keywords
         .none_if_empty()
         .unwrap_or(crate::KEYWORDS.to_owned());
-    let robots = if noindex { "noindex" } else { "all" }.to_string();
+    let robots = if noindex { "noindex" } else { "all" }.to_owned();
+    set_meta(MetaTag::OpenGraph(OpenGraph::Type), &r#type);
     set_title(&title);
+    set_meta(MetaTag::OpenGraph(OpenGraph::Title), &title);
     set_meta(MetaTag::Description, &description);
+    set_meta(MetaTag::OpenGraph(OpenGraph::Description), &description);
     set_meta(MetaTag::Keywords, &keywords);
+    set_meta(MetaTag::OpenGraph(OpenGraph::Image), &image);
     set_meta(MetaTag::Robots, &robots);
+    set_meta(
+        MetaTag::OpenGraph(OpenGraph::SiteName),
+        &crate::TITLE.to_owned(),
+    );
     html! {
         <>
-            <script data-page-content="title" type="text/plain"> { title } </script>
+            <script data-page-content="type" type="text/plain"> { r#type } </script>
+            <script data-page-content="title" type="text/plain"> { title.clone() } </script>
             <script data-page-content="description" type="text/plain"> { description } </script>
             <script data-page-content="keywords" type="text/plain"> { keywords } </script>
+            <script data-page-content="image" type="text/plain"> { image } </script>
             <script data-page-content="robots" type="text/plain"> { robots } </script>
+            <script data-page-content="site_name" type="text/plain"> { crate::TITLE } </script>
         </>
     }
 }
