db0b7b045bb81b66940057022a138a0e6868bc7e
Mikita Bykau <bns.6587@gmail.com>
2025-08-15 18:55:04 +0200

    Guard post actions on user init and simplify edit button

--- src/components/post_card.rs
+++ src/components/post_card.rs
@@ -31,79 +31,74 @@ pub fn post_card(props: &PostCardProps) -> Html {
             }
         });
     }
-    let star_button = match (
-        logged_user_context.is_not_inited(),
-        post.as_ref(),
-        logged_user_context.author(),
-    ) {
-        (false, Some(post), Some(author))
-            if author.editor == 1 && author.blocked == 0 && is_full =>
-        {
-            let recommended_state = recommended.clone();
-            let token = logged_user_context.token().cloned();
-            let post_id = post.id;
-            let onclick = {
-                let recommended_state = recommended_state.clone();
-                let token = token.clone();
-                Callback::from(move |_| {
-                    let Some(token) = token.clone() else {
-                        return;
-                    };
+    let star_button = if logged_user_context.is_not_inited() {
+        html! {}
+    } else {
+        match (post.as_ref(), logged_user_context.author()) {
+            (Some(post), Some(author)) if author.editor == 1 && author.blocked == 0 && is_full => {
+                let recommended_state = recommended.clone();
+                let token = logged_user_context.token().cloned();
+                let post_id = post.id;
+                let onclick = {
                     let recommended_state = recommended_state.clone();
-                    spawn_local(async move {
-                        let res = API::<()>::get(Tokened {
-                            token,
-                            params: PostPoolParams {
-                                id: post_id,
-                                add: !*recommended_state,
-                            },
-                        })
-                        .await;
-                        if let Ok(API::Success { .. }) = res {
-                            recommended_state.set(!*recommended_state);
-                        }
-                    });
-                })
-            };
-            html! {
-                <>
-                    { " " }
-                    <i
-                        class={ classes!("bi", if *recommended { "bi-star-fill" } else { "bi-star" }) }
-                        style="cursor: pointer;"
-                        {onclick}
-                        title={ if *recommended { "Убрать из рекомендаций" } else { "Добавить в рекомендации" } }
-                    ></i>
-                </>
+                    let token = token.clone();
+                    Callback::from(move |_| {
+                        let Some(token) = token.clone() else {
+                            return;
+                        };
+                        let recommended_state = recommended_state.clone();
+                        spawn_local(async move {
+                            let res = API::<()>::get(Tokened {
+                                token,
+                                params: PostPoolParams {
+                                    id: post_id,
+                                    add: !*recommended_state,
+                                },
+                            })
+                            .await;
+                            if let Ok(API::Success { .. }) = res {
+                                recommended_state.set(!*recommended_state);
+                            }
+                        });
+                    })
+                };
+                html! {
+                    <>
+                        { " " }
+                        <i
+                            class={ classes!("bi", if *recommended { "bi-star-fill" } else { "bi-star" }) }
+                            style="cursor: pointer;"
+                            {onclick}
+                            title={ if *recommended { "Убрать из рекомендаций" } else { "Добавить в рекомендации" } }
+                        ></i>
+                    </>
+                }
             }
+            _ => html! {},
         }
-        _ => html! {},
     };
 
-    let edit_button = {
-        if !logged_user_context.is_not_inited() {
-            if let (Some(post), Some(author)) = (post.as_ref(), logged_user_context.author()) {
-                if (author.id == post.author.id || author.editor == 1) && author.blocked == 0 {
-                    html! {
-                        <>
-                            { " " }
-                            <Link<Route, (), Post>
-                                classes="text-decoration-none"
-                                to={ Route::EditPost { id: post.id } }
-                                state={ Some(post.clone()) }
-                            >
-                                <i title="Редактировать публикацию" class="bi bi-pencil-square"></i>
-                            </Link<Route, (), Post>>
-                        </>
-                    }
-                } else {
-                    html! {}
+    let edit_button = if logged_user_context.is_not_inited() {
+        html! {}
+    } else {
+        match (post.as_ref(), logged_user_context.author()) {
+            (Some(post), Some(author))
+                if (author.id == post.author.id || author.editor == 1) && author.blocked == 0 =>
+            {
+                html! {
+                    <>
+                        { " " }
+                        <Link<Route, (), Post>
+                            classes="text-decoration-none"
+                            to={ Route::EditPost { id: post.id } }
+                            state={ Some(post.clone()) }
+                        >
+                            <i title="Редактировать публикацию" class="bi bi-pencil-square"></i>
+                        </Link<Route, (), Post>>
+                    </>
                 }
-            } else {
-                html! {}
             }
-        } else {
-            html! {}
+            _ => html! {},
         }
     };
 
