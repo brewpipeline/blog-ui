49202a212771d16a1008d7e9f2b8610b309fd42f
Mikita Bykau <bns.6587@gmail.com>
2025-08-26 21:02:19 +0200

    style: remove chat page padding

--- src/pages/ai_chat.rs
+++ src/pages/ai_chat.rs
@@ -4,9 +4,5 @@ use crate::components::ai_chat::AiChat;
 
 #[function_component(AiChatPage)]
 pub fn ai_chat_page() -> Html {
-    html! {
-        <div class="container my-4">
-            <AiChat />
-        </div>
-    }
+    html! { <AiChat /> }
 }
